a9e17a6697045314d1201938b6e8f20bdb082e98
FrowningCat <vsavinkov60@gmail.com>
2024-03-15 12:28:00 +0300

    add 'main__registrationForm' in function 'main'

--- src/components/Authorization/Footer/Footer.jsx
+++ src/components/Authorization/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css';
 export default function Fotter() {
     return (
         <div className="footer">
-            <h1>Hello word footer</h1>
+            <h1></h1>
         </div>
     );
 }
--- src/components/Authorization/Main/Main.jsx
+++ src/components/Authorization/Main/Main.jsx
@@ -3,11 +3,146 @@ import './main.css';
 export default function Main() {
     return (
         <main className="main">
-            <div className="main__imgBlock">
-                <h1 className="main__imgBlock__text">
-                    ЗАЯВКА НА УЧАСТИЕ В ПРЕМИИ DAIRYTECH AWARD
-                </h1>
-                <div className="main__imgBlock__img"></div>
+            <div className="main__registrationBlock">
+                <div className="main__registrationBlock__imgBlock">
+                    <h1 className="main__registrationBlock__imgBlock__text">
+                        ЗАЯВКА НА УЧАСТИЕ В ПРЕМИИ DAIRYTECH AWARD
+                    </h1>
+                    <div className="main__registrationBlock__imgBlock__img"></div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__title">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main__registrationBlock__registrationForm">
+                    <div className="main__registrationBlock__registrationForm__block">
+                        <div className="main__registrationBlock__registrationForm__block__form">
+                            <div className="main__registrationBlock__registrationForm__block__form__mainBlock">
+                                <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content">
+                                    <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg">
+                                        <div className="main__registrationBlock__registrationForm__block__form__mainBlock__content__reg__1">
+                                            <span>
+                                                <strong>
+                                                    Информация о компании
+                                                </strong>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     );
