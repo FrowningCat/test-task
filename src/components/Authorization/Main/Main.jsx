3fa18c812455c2ef041b7ca8d8ffbb4a7f31764a
FrowningCat <vsavinkov60@gmail.com>
2024-03-15 11:43:56 +0300

    add 'main__imgBlock' in  function 'main'

--- src/components/Authorization/Main/Main.jsx
+++ src/components/Authorization/Main/Main.jsx
@@ -1,9 +1,14 @@
 import './main.css';
 
-export default function Header() {
+export default function Main() {
     return (
-        <div className="main">
-            <h1>Hello word main</h1>
-        </div>
+        <main className="main">
+            <div className="main__imgBlock">
+                <h1 className="main__imgBlock__text">
+                    ЗАЯВКА НА УЧАСТИЕ В ПРЕМИИ DAIRYTECH AWARD
+                </h1>
+                <div className="main__imgBlock__img"></div>
+            </div>
+        </main>
     );
 }
