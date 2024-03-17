6c22e774e1f3ea165069503bff9f8db9bc610c70
FrowningCat <vsavinkov60@gmail.com>
2024-03-17 16:28:43 +0300

    add scroll

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,12 +1,14 @@
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
+import TopButton from './TopButton/TopButton';
 
 export default function LandingPage() {
     return (
         <>
             <Header />
             <Main />
+            <TopButton />
             <Footer />
         </>
     );
